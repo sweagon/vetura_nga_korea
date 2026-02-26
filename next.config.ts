4d89fbed2db8f803bdf4872d6df714f687fdef3b
Don Diego de la Vega <stonerzorro@outlook.com>
2026-02-26 21:45:00 +0100

    next.config.ts cors mod

--- next.config.ts
+++ next.config.ts
@@ -6,19 +6,8 @@ const nextConfig = {
       {
         source: '/api/:path*',
         headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: 'https://ferrari-export.com' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
         ],
-      },
-    ];
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/cars',
-        destination: 'https://autokoreakosova.com/api/cars',
       },
     ];
   },
